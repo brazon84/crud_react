6d7ea96c98f9ce41e444e2a36493193688c13a52
Jose Gregorio Brazon Lopez <94471674+brazon84@users.noreply.github.com>
2023-05-07 21:53:19 -0300

    tratando que actualize

--- src/App.jsx
+++ src/App.jsx
@@ -3,74 +3,63 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
 import UserList from "./components/userList/UserList";
 import Navbar from "./components/navbar/Navbar";
-import './App.css'
-
-
-
-
+import "./App.css";
 
 function App() {
-  const baseUrl = "https://users-crud.academlo.tech/";
+  const baseUrl = "http://localhost:8080/";
   const [user, setUser] = useState([]);
   const [loadUsers, setLoadUsers] = useState(false);
   const [editUserId, setEditUserId] = useState(null);
-  
- 
-//se realiza la peticion a la api mediante axios
+
+  //se realiza la peticion a la api mediante axios
   const getUser = async () => {
-    try{ 
-      const res =await axios
-      .get(baseUrl + "users/");
-        return(res.data);
+    try {
+      const res = await axios.get(baseUrl + "users/");
+      return res.data;
+    } catch (error) {
+      console.log(error);
     }
-      catch(error){
-        console.log(error);
-      };
   };
 
   const createUser = async (newUsers) => {
     try {
-      const res = await axios.post(baseUrl + "users/", newUsers)
-      loadUser()
+      await axios.post(baseUrl + "users/", newUsers);
+      loadUser();
     } catch (error) {
-      
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
-  const deleteUser = async (userId) => {
+  const deleteUser = async (id) => {
     try {
-      await axios.delete(baseUrl + `users/${userId}/`);
-      alert('Elemento eliminado');
-      const updatedUsers = user.filter((u) => u.id !== userId);
+      await axios.delete(baseUrl + `users/${id}`);
+      alert("Elemento eliminado");
+      const updatedUsers = user.filter((u) => u.id !== id);
       setUser(updatedUsers);
     } catch (error) {
       console.log(error);
     }
   };
-  const editUser = async (Id, updatedUser) => {
+  const editUser = async (id, updatedUser) => {
     try {
-      await axios.put(baseUrl + `users/${Id}/`, updatedUser);
-      setEditUserId(Id);
-      loadUser()
+      await axios.put(baseUrl + `users/${id}`, updatedUser);
+      setEditUserId(null);
+      loadUser();
     } catch (error) {
       console.error(error);
     }
   };
-  
+
   const loadUser = async () => {
     try {
       setLoadUsers(true);
-
       const Users = await getUser();
-setUser(Users)
-setLoadUsers(false)
-
+      setUser(Users);
+      setLoadUsers(false);
     } catch (error) {
       console.error(error);
     }
   };
-
 
   useEffect(() => {
     loadUser();
@@ -78,11 +67,8 @@ setLoadUsers(false)
 
   return (
     <div>
-      <Navbar loadUser={loadUser} createUser={createUser}/>
-      <UserList user={user} deleteUser={deleteUser} editUser={editUser}/>
-  
-     
-         
+      <Navbar loadUser={loadUser} createUser={createUser} />
+      <UserList user={user} deleteUser={deleteUser} editUser={editUser} />
     </div>
   );
 }
